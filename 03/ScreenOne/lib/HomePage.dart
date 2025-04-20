44f7dafe11579ebbd9b51d655b6e7178310938e0
Hanan waleed <waleedhanan81@gmail.com>
2025-04-20 09:34:58 +0200

    Add AppBar

--- 03/ScreenOne/lib/HomePage.dart
+++ 03/ScreenOne/lib/HomePage.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  bool isSwitched = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Switch(
+          value: isSwitched,
+          onChanged: (value) {
+            setState(() {
+              isSwitched = value;
+            });
+          },
+          activeColor: Colors.white,
+          inactiveThumbColor: Colors.grey,
+        ),
+        title: Text(
+          "Sign in",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
     );
   }
 }
