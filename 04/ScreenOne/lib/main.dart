634040c327662cb72f2b813df4fac07cd752b7b3
Hanan waleed <waleedhanan81@gmail.com>
2025-05-12 06:05:32 +0300

    Edit

--- 04/ScreenOne/lib/main.dart
+++ 04/ScreenOne/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Dem',
      
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
